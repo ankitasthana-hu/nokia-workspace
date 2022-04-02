d074920e5cb2b8bbfdb168cb003cf748c2063515
Ankit <ankit.asthana49@gmail.com>
2022-04-02 20:06:00 +0200

    Add Application Code - Flask Web App

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return "Hello, World!"
+    
+@app.route("/salvador")
+def salvador():
+    return "Hello, Salvador"
+    
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True, port=5000)
+    #app.run(debug=True)
