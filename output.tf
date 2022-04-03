d2d9983f8ca0cf01ecdeef920a1326432b58c79c
ankitasthana-hu <59736927+ankitasthana-hu@users.noreply.github.com>
2022-04-03 12:18:27 +0200

    Delete output.tf

--- output.tf
+++
@@ -1,9 +0,0 @@
-output "security_group_id" {
-  description = "The ID of the security group"
-  value       = [ "${aws_security_group.allow-ssh.name}" ]
-}
-
-output "security_group_ids" {
-  description = "The ID of the security group"
-  value       = [ "${aws_security_group.allow-ssh.id}" ]
-}
