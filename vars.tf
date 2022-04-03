a60294c31df24283499c0c89507d352c4cf1700d
ankitasthana-hu <59736927+ankitasthana-hu@users.noreply.github.com>
2022-04-03 06:42:31 +0200

    Create vars.tf

---
+++ vars.tf
@@ -0,0 +1,19 @@
+variable "AWS_REGION" {
+  default = "us-east-1"
+}
+
+variable "PATH_TO_PRIVATE_KEY" {
+  default = "nokia"
+}
+
+variable "PATH_TO_PUBLIC_KEY" {
+  default = "nokia.pub"
+}
+
+variable "sg_name" {
+  default = "securitygroup-standard"
+}
+
+variable "customport" {
+  default = "5000"
+}
